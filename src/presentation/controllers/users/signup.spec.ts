ba7505ccca714bc32d2cba556aa92034f4fc906c
washalbano <washalbano@gmail.com>
2021-04-12 14:28:01 -0300

    refactor: correct test and simplify Users SignUpController

--- src/presentation/controllers/users/signup.spec.ts
+++ src/presentation/controllers/users/signup.spec.ts
@@ -20,9 +20,7 @@ describe('SignUp Controller', () => {
 
     expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
-})
 
-describe('SignUp Controller', () => {
   test('Should return 400 if no email is provided', () => {
     const sut = new SignUpController()
 
@@ -40,5 +38,43 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
 
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'user#1',
+        email: 'user1@email.com',
+        // password: '1234',
+        passwordConfirm: '1234'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+
+  test('Should return 400 if no passwordConfirm is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'user#1',
+        email: 'user1@email.com',
+        password: '1234'
+        // passwordConfirm: '1234'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirm'))
   })
 })
--- src/presentation/controllers/users/signup.ts
+++ src/presentation/controllers/users/signup.ts
@@ -4,12 +4,12 @@ import { HttpRequest, HttpResponse } from '../../protocols/http'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse | any {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirm']
 
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
